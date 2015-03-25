e9bf4cb8d21f428ed7a52f41e8fbd3e364c27b83
Scott Little <uber5001@gmail.com>
2015-03-24 22:49:06 -0400

    add 2s delay with calibration

--- server.js
+++ server.js
@@ -8,6 +8,29 @@ wss.broadcast = function broadcast(data) {
 
 wss.on('connection', function connection(ws) {
 	ws.on('message', function incoming(message) {
-		wss.broadcast(message);
+		try {
+			message = JSON.parse(message);
+		} catch (e) {
+			//console.log(e);
+			return;
+		}
+		if (responses[message.type]) {
+			responses[message.type](ws, message);
+		}
 	});
 });
+
+var responses = {
+	"time": function(ws, message) {
+		ws.send(JSON.stringify({
+			type: "time",
+			data: Date.now() //time in ms
+		}));
+	},
+	"note": function(ws, message) {
+		wss.broadcast(JSON.stringify({
+			type: "note",
+			data: message.data
+		}));
+	}
+}
