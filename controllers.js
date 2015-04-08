dcbd3daf3b2b5e103d5cadb38e16e001af09356e
Scott Little <uber5001@gmail.com>
2015-04-08 17:26:10 -0400

    add Keyboard object; add chords

--- controllers.js
+++ controllers.js
@@ -13,6 +13,9 @@ function showScreen(screenName) {
 		document.body.classList.remove(screenIds[i]);
 	}
 	document.body.classList.add(screenName);
+	if (screenName == "server-screen") {
+		$("#chord-editor").scrollLeft = 980;
+	}
 }
 
 //loading-screen
@@ -58,6 +61,8 @@ $("#join-code-submit").onclick = function() {
 
 
 //server-screen
+//chord-editor
+//generate keyboard
 // spacings gotten from: http://www.quadibloc.com/other/cnv05.htm
 var octaveWidth = 7*35;
 var blackPercentHeight = .6;
@@ -67,14 +72,71 @@ for (var i = 0; i < 127; i++) {
 	
 	var element = document.createElement('div');
 	element.setAttribute('data-octave', octave);
+	element.classList.add("key");
+	if (note == 1 || note == 3 || note == 6 || note == 8 || note == 10)
+		element.classList.add("black");
+	else
+		element.classList.add("white");
 	element.classList.add("key-" + note);
 	element.classList.add("octave-" + octave);
+	element.setAttribute("id","note-" + i);
 	
 	element.onclick = (function() {
 		var midiValue = i;
 		return function() {
-			alert('Not yet implemented! DEBUG: ' + midiValue);
+			//on press of piano key
+			Keyboard.toggle(midiValue);
 		}
 	})();
 	$("#chord-editor").appendChild(element);
 }
+
+var Keyboard = new function() {
+	var current = {};
+	this.onchange = function() {}
+	this.toggle = function(note) {
+		$("#note-" + note).classList.toggle("active");
+		if (current[note])
+			delete current[note]
+		else
+			current[note] = true;
+		this.onchange();
+	}
+	this.getNotes = function() {
+		var notes = [];
+		for (var i in current)
+			note.push(i);
+		return notes;
+	}
+	this.setNotes = function(array) {
+		for (var i in current)
+			$("#note-" + i).classList.toggle("active");
+		var current = {};
+		for (var i = 0; i < array.length; i++)
+			this.toggle(array[i]) = true;
+	}
+}();
+
+//chord-list
+$("#new-chord").onclick = function() {
+	var chord = {
+		name: "untitled chord",
+		notes: []
+	}
+
+	var item = document.createElement('li');
+	var link = document.createElement('a');
+	
+	link.innerHTML = chord.name;
+	link.setAttribute("href", "");
+	link.onclick = function(e) {
+		e.preventDefault();
+		//show chord
+		
+	}
+	
+	item.appendChild(link);
+	$("#chord-list ul").appendChild(item);
+}
+
+
